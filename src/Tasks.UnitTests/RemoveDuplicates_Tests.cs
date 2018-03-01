a49779abb36311db30cebca0a65a660fbe8594ec
Christopher Warrington <chwarr@microsoft.com>
2018-02-28 19:09:50 -0800

    Add HadAnyDuplicates output to RemoveDuplicates task
    The `HadAnyDuplicates` output indicates whether the input list contained
    any duplicates. This makes it easier to implement checks that Items
    don't contain any duplicates (e.g., to error if duplicate compilation
    would occur).
    
    Fixes https://github.com/Microsoft/msbuild/issues/1541

--- src/Tasks.UnitTests/RemoveDuplicates_Tests.cs
+++ src/Tasks.UnitTests/RemoveDuplicates_Tests.cs
@@ -26,7 +26,9 @@ namespace Microsoft.Build.UnitTests
 
             bool success = t.Execute();
             Assert.True(success);
+            Assert.Equal(1, t.Filtered.Length);
             Assert.Equal("MyFile.txt", t.Filtered[0].ItemSpec);
+            Assert.False(t.HadAnyDuplicates);
         }
 
         /// <summary>
@@ -42,7 +44,9 @@ namespace Microsoft.Build.UnitTests
 
             bool success = t.Execute();
             Assert.True(success);
+            Assert.Equal(1, t.Filtered.Length);
             Assert.Equal("MyFile.txt", t.Filtered[0].ItemSpec);
+            Assert.True(t.HadAnyDuplicates);
         }
 
         /// <summary>
@@ -58,8 +62,10 @@ namespace Microsoft.Build.UnitTests
 
             bool success = t.Execute();
             Assert.True(success);
+            Assert.Equal(2, t.Filtered.Length);
             Assert.Equal("MyFile1.txt", t.Filtered[0].ItemSpec);
             Assert.Equal("MyFile2.txt", t.Filtered[1].ItemSpec);
+            Assert.False(t.HadAnyDuplicates);
         }
 
         /// <summary>
@@ -75,7 +81,9 @@ namespace Microsoft.Build.UnitTests
 
             bool success = t.Execute();
             Assert.True(success);
+            Assert.Equal(1, t.Filtered.Length);
             Assert.Equal("MyFile.txt", t.Filtered[0].ItemSpec);
+            Assert.True(t.HadAnyDuplicates);
         }
 
         /// <summary>
@@ -90,9 +98,7 @@ namespace Microsoft.Build.UnitTests
 
             Assert.True(success);
             Assert.Equal(0, t.Filtered.Length);
+            Assert.False(t.HadAnyDuplicates);
         }
     }
 }
-
-
-
